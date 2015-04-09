196eff882d9350d034a1b13fbb1b1937211bc02c
Andre Torgal <andre.torgal@gmail.com>
2015-04-09 01:26:15 +0100

    first commit

---
+++ dist/lib/ng.cork.ui.js
@@ -0,0 +1,10 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('ng.cork.ui.focus-on', [
+        'ng.cork.ui.focus-on.enter-click',
+        'ng.cork.ui.focus-on',
+        'ng.cork.ui.focus-on.stop-propagation'
+    ]);
+
+})(angular);
---
+++ src/lib/ng.cork.ui.focus-on/focusOn.spec.js
@@ -0,0 +1,14 @@
+describe('ng.cork.ui.focus-on', function () {
+    'use strict';
+
+    beforeEach(module('ng.cork.ui.focus-on'));
+
+    describe('corkUiFocusOn', function () {
+
+        it('should pass a dummy test.', inject(function () {
+            expect(true).toBe(true);
+        }));
+
+    });
+
+});
